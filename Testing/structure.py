5f3ae3e50dd89863848ea86811a56836f6db3578
WhiteFall <1947114574@qq.com>
2023-11-04 17:25:15 +0800

    fix something

---
+++ Testing/structure.py
@@ -0,0 +1,29 @@
+datab = [
+    {
+        "ID": 1,
+        "Title": "书籍名称",
+        "Type": "Concept",
+        "SuperMemoElement": [
+            {
+                "ID": 2,
+                "Title": "标题",
+                "Type": "Topic",
+                "Content": {"Question": "问题", "Answer": "答案"},
+            },
+            {
+                "ID": 3,
+                "Title": "标题",
+                "Type": "Topic",
+                "Content": {"Question": "问题", "Answer": "答案"},
+                "SuperMemoElement": [
+                    {
+                        "ID": 4,
+                        "Title": "标题",
+                        "Type": "Topic",
+                        "Content": {"Question": "问题", "Answer": "答案"},
+                    }
+                ],
+            },
+        ],
+    }
+]
--- index.py
+++ index.py
@@ -29,53 +29,17 @@ def create_xml(data):
         create_supermemo_element(root, element_data)
 
     tree = ET.ElementTree(root)
-    tree.write("output/example.xml", encoding="utf-8", xml_declaration=True)
+    tree.write("output/11example.xml", encoding="utf-8", xml_declaration=True)
 
 
 def count_ids(data):
     count = 0
-    
     for item in data:
-        if 'ID' in item:
+        if "ID" in item:
             count += 1
-        
-        if 'SuperMemoElement' in item:
-            count += count_ids(item['SuperMemoElement'])
-    
+        if "SuperMemoElement" in item:
+            count += count_ids(item["SuperMemoElement"])
     return count
-
-datab = [
-    {
-        "ID": 1,
-        "Title": "书籍名称",
-        "Type": "Concept",
-        "SuperMemoElement": [
-            {
-                "ID": 2,
-                "Title": "标题",
-                "Type": "Topic",
-                "Content": {"Question": "问题", "Answer": "答案"},
-            },
-            {
-                "ID": 3,
-                "Title": "标题",
-                "Type": "Topic",
-                "Content": {"Question": "问题", "Answer": "答案"},
-                "SuperMemoElement": [
-                    {
-                        "ID": 4,
-                        "Title": "标题",
-                        "Type": "Topic",
-                        "Content": {"Question": "问题", "Answer": "答案"},
-                    }
-                ],
-            },
-        ],
-    }
-]
-
-book = epub.read_epub("epubs/聪明人的个人成长.epub")
-toc = book.toc
 
 
 def get_documents(chapters, Id=1):
@@ -90,36 +54,45 @@ def get_documents(chapters, Id=1):
             doc = book.get_item_with_href(href)
             if doc:
                 Content = {"Question": doc.content.decode("utf-8")}  # 获取文档内容
-                mList.append(
-                    {"Title": title, "ID": Id, "Type": "Topic", "Content": Content}
-                )
+            else:
+                Content = {"Question": ""}
+            mList.append(
+                {"Title": title, "ID": Id, "Type": "Topic", "Content": Content}
+            )
+        # 是元组的时候就说明是有子集的数据。这个元组代表当前的数据。元组的内容代表下一层数据。
+        # 在元组中，其中第一个元素是本层的数据，第二个元素是下一层的数据，也是入口。
         if isinstance(chapter, tuple):
             Id += 1
             if isinstance(chapter[0], epub.Section):
                 title = chapter[0].title
                 href = chapter[0].href
                 doc = book.get_item_with_href(href)
+                # 暂时忽略锚点的情况:'Text/Section0001_0012.xhtml#toc_1
                 if doc:
                     Content = {"Question": doc.content.decode("utf-8")}  # 获取文档内容
-                    element = {
-                        "Title": title,
-                        "ID": Id,
-                        "Type": "Topic",
-                        "Content": Content,
-                        # "SuperMemoElement": SuperMemoElement,
-                    }
-                    mList.append(element)
-            # 当有子元素的时候。
-            if isinstance(chapter[1], list) and len(chapter[1]) > 0:
-                element = {}
+                else:
+                    Content = {"Question": ""}
+                element = {
+                    "Title": title,
+                    "ID": Id,
+                    "Type": "Topic",
+                    "Content": Content,
+                    # "SuperMemoElement": SuperMemoElement,
+                }
+                mList.append(element)
+            # 当元组的第二个元素有子元素的时候。
+            if isinstance(chapter[1], list):
                 SubElementList = get_documents(chapter[1])
                 element["SuperMemoElement"] = SubElementList
     return mList
+
+
+book = epub.read_epub("epubs/心理学与生活.epub")
 
 res = get_documents(book.toc)
 
 data = []
 
-data.append({"ID": 1, "Title": book.get_metadata("DC", "title")[0][0], "Type": "Concept", "SuperMemoElement": res})
+data.append({"ID": 1, "Title": book.title, "Type": "Concept", "SuperMemoElement": res})
 
 create_xml(data)
