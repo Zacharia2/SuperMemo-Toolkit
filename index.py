91d83cd856eb1ce8ad736f4609af72cd18966d95
WhiteFall <1947114574@qq.com>
2023-11-04 09:25:18 +0800

    start

--- index.py
+++ index.py
@@ -1,4 +1,8 @@
+from datetime import date
+from unittest import result
 import xml.etree.ElementTree as ET
+import ebooklib
+from ebooklib import epub
 
 
 def create_supermemo_element(parent_element, element_data):
@@ -31,7 +35,7 @@ def create_xml(data):
     tree.write("example.xml", encoding="utf-8", xml_declaration=True)
 
 
-data = [
+datab = [
     {
         "ID": 1,
         "Title": "书籍名称",
@@ -60,5 +64,53 @@ data = [
         ],
     }
 ]
+
+book = epub.read_epub("聪明人的个人成长.epub")
+toc = book.toc
+
+
+def get_documents(chapters, Id=1):
+    mList = []
+    for chapter in chapters:
+        # 把这一层处理好，再去处理下一层。
+        # 这一层和下一层的逻辑一致。
+        if isinstance(chapter, epub.Link):
+            Id += 1
+            title = chapter.title
+            href = chapter.href
+            doc = book.get_item_with_href(href)
+            if doc:
+                Content = {"Question": doc.content.decode("utf-8")}  # 获取文档内容
+                mList.append(
+                    {"Title": title, "ID": Id, "Type": "Topic", "Content": Content}
+                )
+        if isinstance(chapter, tuple):
+            Id += 1
+            if isinstance(chapter[0], epub.Section):
+                title = chapter[0].title
+                href = chapter[0].href
+                doc = book.get_item_with_href(href)
+                if doc:
+                    Content = {"Question": doc.content.decode("utf-8")}  # 获取文档内容
+                    element = {
+                        "Title": title,
+                        "ID": Id,
+                        "Type": "Topic",
+                        "Content": Content,
+                        # "SuperMemoElement": SuperMemoElement,
+                    }
+                    mList.append(element)
+            # 当有子元素的时候。
+            if isinstance(chapter[1], list) and len(chapter[1]) > 0:
+                element = {}
+                SubElementList = get_documents(chapter[1])
+                element["SuperMemoElement"] = SubElementList
+    return mList
+
+res = get_documents(book.toc)
+
+data = []
+
+data.append({"ID": 1, "Title": "书籍名称", "Type": "Concept", "SuperMemoElement": res})
 
 create_xml(data)
