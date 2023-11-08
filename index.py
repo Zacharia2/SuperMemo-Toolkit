6f202a0bf8ebfc18e81b0cbac44ab6818bd92c21
WhiteFall <1947114574@qq.com>
2023-11-08 21:32:59 +0800

    test and learn  docopt

--- index.py
+++ index.py
@@ -1,14 +1,14 @@
 # encoding:utf-8
-""" CLI Demo Tool
+"""SMKit CLI Tool
 Usage:
-    clidemo cmdA <valueA>
-    clidemo cmdB paramB <valueB>
+    smkit test <valueA>
+    smkit e2s <valueA> <valueB>
 
 Options:
     -h --help       Show Help doc.
     -v --version    Show Version.
 """
-# import pkg
+
 import os
 from docopt import docopt
 from epub2sm import epub2sm
@@ -16,12 +16,16 @@ from epub2sm import epub2sm
 
 def cmd():
     args = docopt(__doc__)
-    if args.get("cmdA"):
+    if args.get("test"):
+        # python index.py e2s a
         print("#" * 100)
         print(args["<valueA>"])
         print("#" * 100)
         epub2sm.test()
-
+    elif args.get("e2s"):
+        epub2sm.test()
+        # python index.py e2s a b
+        pass
 
 
 if __name__ == "__main__":
