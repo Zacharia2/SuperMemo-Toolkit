612d339ed5ca1d66ca1178ff87822478b3b9030c
WhiteFall <1947114574@qq.com>
2023-11-10 16:34:21 +0800

    maybe fix 编码
    maybe fix 编码
    
    maybe fix 编码
    
    import codecs
    
    Update index.py

--- pathpix/index.py
+++ pathpix/index.py
@@ -1,4 +1,4 @@
-import html
+import codecs
 import shutil
 import uuid
 from bs4 import BeautifulSoup
@@ -15,45 +15,6 @@ def mkdir(path):
     if not folder:
         os.makedirs(path)
         print("创建文件夹:: " + path)
-
-
-def is_utf8_supported_in_gbk(char):
-    code = ord(char)
-    try:
-        chr(code).encode("gbk")
-        return True
-    except ValueError:
-        print(
-            f"Unicode code point: {code}",
-            f"无法找到对应的GBK字符, 将使用字符实体 (numeric character reference) '&#x{code};' 替代",
-        )
-        return (chr(code), f"&#x{code};")
-
-
-# 最好可以自动将不支持的字符转换为字符实体。
-# 挨个读取字符，判断gb2312是否支持，不支持就变成字符实体。
-def make_escape_safe(html_str):
-    escapeSequence = {
-        "EM SPACE": (chr(0x2003), "&ensp;"),
-        "COPYRIGHT SIGN": (chr(0x00A9), "&copy;"),
-        "EM DASH": (chr(0x2014), "&#8212;"),
-        "chapterlast": (chr(0xF108), "&#10048;"),
-        "REPLACEMENT CHARACTER": (chr(0xFFFD), "&#65533;"),
-        "WHITE FLORETTE": (chr(0x2740), "&#10048;"),
-        "START OF SELECTED AREA": (chr(0x0086), "&#x0086;"),
-    }
-    for escape in escapeSequence.values():
-        html_str = html_str.replace(escape[0], escape[1])
-
-    nosupp_char_list = []
-    for char in html_str:
-        result_bool = is_utf8_supported_in_gbk(char)
-        if not result_bool:
-            nosupp_char_list.append(result_bool)
-
-    for char_tuple in nosupp_char_list:
-        html_str = html_str.replace(char_tuple[0], char_tuple[1])
-    return html_str
 
 
 def is_in_elements_directory(path, directory):
@@ -210,7 +171,7 @@ def modify_src(html_doc, im_saved_path, elements_path, collection_temp_path):
     # print("删除临时文件夹::", collection_temp_path)
     # shutil.rmtree(collection_temp_path)
     if is_modify:
-        return make_escape_safe(str(soup))
+        return soup.encode(encoding="ascii")
     else:
         return False
 
@@ -234,16 +195,16 @@ def relative_and_localize(elements_path, web_im_saved_path, collection_temp_path
                 if entry.is_file() and is_html_file(entry.name):
                     # todo something
                     try:
-                        with open(entry.path, "r+", encoding="GBK") as f:
-                            # 转义序列转换为字符。
-                            unescape_content = html.unescape(f.read())
-                            modified_content = modify_src(
-                                unescape_content,
-                                web_im_saved_path,
-                                elements_path,
-                                collection_temp_path,
-                            )
-                            if modified_content:
+                        with codecs.open(entry.path, "r", encoding="gbk") as f:
+                            content = f.read()
+                        modified_content = modify_src(
+                            content,
+                            web_im_saved_path,
+                            elements_path,
+                            collection_temp_path,
+                        )
+                        if modified_content:
+                            with codecs.open(entry.path, "wb") as f:
                                 print("正在处理：", entry.path)
                                 f.seek(0)
                                 f.write(modified_content)
@@ -265,8 +226,8 @@ def relative_and_localize(elements_path, web_im_saved_path, collection_temp_path
     return failed_process_htm_files
 
 
-relative_and_localize(
-    "C:/Users/Snowy/Desktop/sm18/systems/ZiBenLun(JiNianBan)QuanSanJuan/elements",
-    "C:/Users/Snowy/Desktop/sm18/systems/ZiBenLun(JiNianBan)QuanSanJuan/elements/web_pic",
-    "C:/Users/Snowy/Desktop/sm18/systems/ZiBenLun(JiNianBan)QuanSanJuan/temp",
-)
+# relative_and_localize(
+#     "C:/Users/Snowy/Desktop/sm18/systems/all in one/elements",
+#     "C:/Users/Snowy/Desktop/sm18/systems/all in one/elements/web_pic",
+#     "C:/Users/Snowy/Desktop/sm18/systems/all in one/temp",
+# )
