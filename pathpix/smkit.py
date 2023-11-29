be806bb2db1c62a598f3cd3f41fcdbfad765e4b3
WhiteFall <1947114574@qq.com>
2023-11-29 22:10:06 +0800

    build tree

--- pathpix/smkit.py
+++ pathpix/smkit.py
@@ -11,11 +11,13 @@ Options:
     -h --help       Show Help doc.
     -v --version    Show Version.
 """
-import docs.conf
 from docopt import docopt
-from pathpix import index as pathpix
+import index as pathpix
 import os
+import sys
 
+sys.path.insert(0, sys.path[0] + "/../")
+from scripts import config  # noqa: E402
 
 # pyinstaller --add-data "conf.json;." smkit.py
 
@@ -24,29 +26,29 @@ def cmd():
     args = docopt(__doc__)
     this_folder = os.path.dirname(os.path.abspath(__file__))
     conf_path = os.path.join(this_folder, "conf.json")
-    m_conf = docs.conf.read_config(conf_path)
+    m_conf = config.read_config(conf_path)
     sm_location = m_conf["program"]
 
     if args.get("config") and args.get("set"):
         if args["<key>"] == "program":
             m_conf["program"] = args["<value>"]
-            docs.conf.update_config(conf_path, m_conf)
-            m_conf = docs.conf.read_config(conf_path)
+            config.update_config(conf_path, m_conf)
+            m_conf = config.read_config(conf_path)
             print(m_conf)
         else:
             m_conf[args["<key>"]] = args["<value>"]
-            docs.conf.update_config(conf_path, m_conf)
-            m_conf = docs.conf.read_config(conf_path)
+            config.update_config(conf_path, m_conf)
+            m_conf = config.read_config(conf_path)
             print(m_conf)
 
     elif args.get("config") and args.get("list"):
-        print(docs.conf.read_config(conf_path))
+        print(config.read_config(conf_path))
 
     elif args.get("pathpix"):
-        col_folder = docs.conf.get_collection_primaryStorage(
+        col_folder = config.get_collection_primaryStorage(
             sm_location, args["<collection>"]
         )
-        collection_temp_path = docs.conf.get_collections_temp(
+        collection_temp_path = config.get_collections_temp(
             os.path.join(sm_location, "systems", args["<collection>"])
         )
         save_img_folder = os.path.join(col_folder, "web_pic")
@@ -56,7 +58,7 @@ def cmd():
         print("临时文件：", collection_temp_path)
         pathpix.start(col_folder, save_img_folder, collection_temp_path)
     elif args.get("clist"):
-        col_list = docs.conf.get_collections_primaryStorage(sm_location)
+        col_list = config.get_collections_primaryStorage(sm_location)
         for col_name in col_list:
             print("集合名称：", col_name)
 
