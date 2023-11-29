41b4b18387adec29757d1c72da6ff0b5f584bd4e
WhiteFall <1947114574@qq.com>
2023-11-29 22:10:06 +0800

    build tree

--- scripts/config.py
+++ scripts/config.py
@@ -66,3 +66,31 @@ def get_collections_temp(collection_path):
         _type_: drive:/path/sm/systems/col/temp
     """
     return os.path.join(collection_path, "temp")
+
+
+# 引入configparser包，它是用于读取配置文件的包
+# 配置文件的格式可以为[](其中包含的为section)
+
+
+# 获取config.ini文件路径
+def get_path():
+    current_path = os.path.abspath(__file__)
+    return os.path.join(
+        os.path.abspath(os.path.dirname(current_path) + os.path.sep), "config.ini"
+    )
+
+
+# 定义读取配置文件的函数，分别读取section中的配置参数
+
+
+def get_config(config_file=get_path()):
+    parser = configparser.ConfigParser()
+    parser.read(config_file)
+    # 获取整型参数，按照key-value的形式保存
+    _conf_ints = [(key, int(value)) for key, value in parser.items("ints")]
+
+    # 获取字符型参数，按照key-value的形式保存
+    _conf_strings = [(key, str(value)) for key, value in parser.items("strings")]
+
+    # 返回一个字典对象，包含读取的参数
+    return dict(_conf_ints + _conf_strings)
