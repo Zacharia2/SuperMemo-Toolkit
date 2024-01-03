6617090cef63ae9078b790964e54b5d7e7f9fb14
WhiteFall <1947114574@qq.com>
2024-01-03 20:18:48 +0800

    fix some error

--- epub2sm/toc_units/check_toc.py
+++ epub2sm/toc_units/check_toc.py
@@ -4,26 +4,22 @@ from ebooklib import epub
 
 def get_doc_of_toc(chapters):
     mList = []
-
-    def iter_toc(chapters):
-        nonlocal mList
+    stack = [chapters]
+    while stack:
+        chapters = stack.pop()
         for chapter in chapters:
             if isinstance(chapter, epub.Link):
                 # title = chapter.title
-                href = chapter.href
-                file_name = href.split("#")[0]
+                file_name = chapter.href.split("#")[0]
                 mList.append(file_name)
             if isinstance(chapter, tuple):
                 if isinstance(chapter[0], epub.Section):
                     # title = chapter[0].title
-                    href = chapter[0].href
-                    file_name = href.split("#")[0]
+                    file_name = chapter[0].href.split("#")[0]
                     mList.append(file_name)
                 # 当元组的第二个元素有子元素的时候。
                 if isinstance(chapter[1], list):
-                    iter_toc(chapter[1])
-
-    iter_toc(chapters)
+                    stack.append(chapter[1])
     return mList
 
 
@@ -50,13 +46,20 @@ def contrast_diff(epubfile):
 
 
 def contrast_diff_toc(toc, book):
-    """
-    docstring
+    """对比toc中的文档是否都在doc_items中。toc取文件名与doc_items进行对比。
+
+    Args:
+        toc (_type_): _description_
+        book (_type_): _description_
+
+    Returns:
+        _type_: 缺失的toc文档列表。
     """
     toc_of_set = set(get_doc_of_toc(toc))
     doc_of_href_set = set(get_doc_items_href(book))
 
-    # 返回一个集合，元素包含在集合 x ，但不在集合 y
+    # 返回一个集合，元素包含在集合 doc_of_href_set ，但不在集合 toc_of_set
+    # doc_of_href_set是全集。
     diff_list = list(doc_of_href_set.difference(toc_of_set))
     return diff_list
 
