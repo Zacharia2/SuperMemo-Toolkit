4c61b5f943fa086a4cf5c463af43c7c145bb6a4d
Zacharia2 <32425955+Zacharia2@users.noreply.github.com>
2024-02-24 14:45:39 +0800

    Update web_import.py

--- anki/web_import.py
+++ anki/web_import.py
@@ -1,7 +1,9 @@
 import os
+import time
 import requests
 from readability import Document
 import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 
 
 class Gen_SM_BookXML:
@@ -71,32 +73,54 @@ class Gen_SM_BookXML:
                     stack.append(sub_element)
 
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
-}
-
-
 # with open("pathpix/weblist.txt", "r", encoding="utf-8") as f:
 #     lines = f.readlines()
 # 这个是Collection下的第一层SuperMemoElement
 
-lines = [
-    "https://blog.csdn.net/cqcre/article/details/100681678",
-    "https://github.com/andreasvc/readability/",
-]
+# lines = [
+#     "https://blog.csdn.net/cqcre/article/details/100681678",
+#     "https://github.com/andreasvc/readability/",
+# ]
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
+}
+
+start = time.time()
+with open("C:/Users/Snowy/Desktop/favorites_2024_2_24.html", "rb") as f:
+    raw = f.read()
+    soup = BeautifulSoup(raw, "html.parser")
+    a_link_list = soup.find_all("a")
+    link_list = [x.attrs["href"] for x in a_link_list if "href" in x.attrs]
+
+print(f"WebI::检测到 {len(link_list)} 条链接")
 rootElement = []
-for line in lines:
-    response = requests.get(line, stream=True, headers=headers)
-    doc = Document(response.content)
-    element = {
-        "Title": doc.title(),
-        "Type": "Topic",
-        "Content": {"Question": str(doc.summary(html_partial=True))},
-    }
-    rootElement.append(element)
+for link in link_list:
+    try:
+        response = requests.get(link, stream=True, headers=headers)
+        content_type = response.headers.get("content-type")
+        content = response.content
+        doc = Document(response.content)
+        element = {
+            "Title": doc.title(),
+            "Type": "Topic",
+            "Content": {"Question": str(doc.summary(html_partial=True))},
+        }
+        rootElement.append(element)
+        print(link)
+    except requests.exceptions.ConnectionError as e:
+        print("网络连接异常: ", e)
+    except requests.exceptions.Timeout as e:
+        print("连接超时: ", e)
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP错误, 状态码: {e.response.status_code}, {e}")
+    except ValueError as e:
+        print("响应解析异常: ", e)
+    except Exception:
+        pass
 
 webfile = Gen_SM_BookXML("websmxml")
-
+end = time.time()
+print("WebI:: Done!", f" 耗时 {end - start:.2f} 秒。")
 # 根据数据结构创建XML文件
 webfile.create_xml(rootElement)
