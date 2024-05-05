74efa5550b3d21f6a5c6551ff87fd0fab2a2d6b2
Zacharia2 <32425955+Zacharia2@users.noreply.github.com>
2024-05-05 12:39:39 +0800

    up

--- tests/anki/sent.py
+++ tests/anki/sent.py
@@ -11,19 +11,19 @@ from main import invoke  # noqa: E402
 df = pd.read_excel("C:/Users/Snowy/Desktop/2000.xlsx", sheet_name="Sheet1")
 # word_list = df.iloc[:, 0].to_dict()
 # sent_dataFrame = df.iloc[1:60, 0:4].to_dict()
-sent_dataFrame = [row.to_dict() for index, row in df.iloc[0:60, 0:4].iterrows()]
-aa_sent_dataFrame = [row.to_dict() for index, row in df.iloc[60:100, 0:3].iterrows()]
+# sent_dataFrame = [row.to_dict() for index, row in df.iloc[0:60, 0:4].iterrows()]
+aa_sent_dataFrame = [row.to_dict() for index, row in df.iloc[0:100, 0:3].iterrows()]
 # word_list = [x.strip() for x in sent_dataFrame["词集"] if isinstance(x, str)]
 
-for index, sent in enumerate(sent_dataFrame):
-    words = str(sent["词集"]).split("、")
-    sent_dataFrame[index]["词集"] = words
+# for index, sent in enumerate(sent_dataFrame):
+#     words = str(sent["词集"]).split("、")
+#     sent_dataFrame[index]["词集"] = words
 
 for index, sent in enumerate(aa_sent_dataFrame):
     matches = [x for x in re.findall(r"\[(.*?)\]", sent["句子"])]
     aa_sent_dataFrame[index]["词集"] = matches
 
-sent_list = sent_dataFrame + aa_sent_dataFrame
+# sent_list = sent_dataFrame + aa_sent_dataFrame
 
 note_id_list = invoke("findNotes", query="deck:2024红宝书考研词汇")
 notesInfo = invoke("notesInfo", notes=note_id_list)
@@ -34,17 +34,17 @@ for index, noteInfo in enumerate(notesInfo):
     tags: list = noteInfo["tags"]
     fields = noteInfo["fields"]
     word = fields["word"]["value"]
-    for sigsent in sent_list:
+    for sigsent in aa_sent_dataFrame:
         for cword in sigsent["词集"]:
             if cmp_2word(word, cword):
                 count += 1
                 replaced_text = re.sub(
-                    r"\[" + cword + r"\]",
-                    f'<font color="red"><b>{cword}</b></font>',
+                    r"\[.{0,2}" + cword + r".{0,2}\]",
+                    f'<span class="curr_word"><b>{cword}</b></span>',
                     sigsent["句子"],
                 )
                 replaced_text = re.sub(
-                    r"\[(.*?)\]", r'<font color="blue">\1</font>', replaced_text
+                    r"\[(.*?)\]", r'<span class="other_word">\1</span>', replaced_text
                 )
                 num = int(sigsent["序列"])
                 invoke(
