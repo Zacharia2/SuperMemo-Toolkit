04be0bab514792b423216e38b207f450b8a5c135
Zacharia2 <32425955+Zacharia2@users.noreply.github.com>
2024-06-23 08:49:58 +0800

    up structure sm xml

--- supermemo_toolkit/epub2sm/smxml.py
+++ supermemo_toolkit/epub2sm/smxml.py
@@ -1,70 +1,71 @@
 import os
-import xml.etree.ElementTree as Et
+from typing import Any
+from xml.etree.ElementTree import Element, SubElement, ElementTree
 
 import ebooklib
 from ebooklib import epub
 
 from supermemo_toolkit.utilscripts.ulils import mkdir
+
+
 # sys.path.insert(0, os.path.normpath(sys.path[0] + "/../../"))
 
 
 class SmXml:
-    def __init__(self, ebook: epub.EpubBook, book_name: str, save_folder=os.getcwd()):
-        self.ebook = ebook
-        self.save_folder = save_folder
-        self.book_name = book_name
-        self.book_file_path = os.path.join(self.save_folder, self.book_name + ".xml")
-        self.book_img_folder = os.path.join(self.save_folder, self.book_name)
+    def __init__(self, book_name: str, save_folder=os.getcwd(), **kwargs):
+        self.save_folder: str = save_folder
+        self.book_name: str = book_name
+        self.ebook: epub.EpubBook = kwargs["ebook"]
+        self.book_file_path: str = os.path.join(self.save_folder, self.book_name + ".xml")
+        self.book_img_folder: str = os.path.join(self.save_folder, self.book_name)
 
-    def create_xml(self, data):
+    def create_xml(self, data: list[dict[str, Any]]):
         """输入data, 创建SuperMemo-XML-Book
 
         Args:
             data (_type_): _description_
         """
 
-        root = Et.Element("SuperMemoCollection")
-
-        count = Et.SubElement(root, "Count")
+        root = Element("SuperMemoCollection")
+        count = SubElement(root, "Count")
         self.set_unique_id(data)
-        count.text = str(self.count_ids(data))
-
-        for element_data in data:
-            self.create_supermemo_element(root, element_data)
-
-        tree = Et.ElementTree(root)
+        count.text = self.count_ids(data)
+        self.create_supermemo_element(root, data)
+        tree = ElementTree(root)
         tree.write(self.book_file_path, encoding="utf-8", xml_declaration=True)
+        if self.ebook:
+            self.write_img_file(self.ebook)
 
-        self.write_img_file()
+    def create_supermemo_element(self, parent_element: Element, data: list[dict[str, Any]]) -> None:
+        supermemo_element = SubElement(parent_element, "SuperMemoElement")
+        for element_data in data:
+            for key, value in element_data.items():
+                if key == "SuperMemoElement":
+                    # SuperMemoElement是一个数组
+                    self.create_supermemo_element(supermemo_element, value)
+                elif key == "Content":
+                    content = SubElement(supermemo_element, "Content")
+                    for sub_key, sub_value in value.items():
+                        sub_element = SubElement(content, sub_key)
+                        sub_element.text = str(sub_value)
+                else:
+                    sub_element = SubElement(supermemo_element, key)
+                    sub_element.text = str(value)
 
-    def create_supermemo_element(self, parent_element, element_data):
-        supermemo_element = Et.SubElement(parent_element, "SuperMemoElement")
-
-        for key, value in element_data.items():
-            if key == "SuperMemoElement":
-                # SuperMemoElement是一个数组
-                for sub_element_data in value:
-                    self.create_supermemo_element(supermemo_element, sub_element_data)
-            elif key == "Content":
-                content = Et.SubElement(supermemo_element, "Content")
-                for sub_key, sub_value in value.items():
-                    sub_element = Et.SubElement(content, sub_key)
-                    sub_element.text = str(sub_value)
-            else:
-                sub_element = Et.SubElement(supermemo_element, key)
-                sub_element.text = str(value)
-
-    def count_ids(self, data):
+    @staticmethod
+    def count_ids(data: list[dict[str, Any]]) -> str:
         count = 0
-        for item in data:
+        stack = list(data)  # 将初始数据转换为栈
+        while stack:
+            item = stack.pop()  # 从栈中取出一个元素
             if "ID" in item:
                 count += 1
             if "SuperMemoElement" in item:
-                count += self.count_ids(item["SuperMemoElement"])
-        return count
+                stack.extend(item["SuperMemoElement"])  # 将嵌套的列表扩展到栈中
+        return str(count)
 
     @staticmethod
-    def set_unique_id(data: list):
+    def set_unique_id(data: list[dict[str, Any]]):
         """为SuperMemo元素设置id
 
         Args:
@@ -81,9 +82,9 @@ class SmXml:
                 for sub_element in current_element["SuperMemoElement"]:
                     stack.append(sub_element)
 
-    def write_img_file(self):
+    def write_img_file(self, ebook: epub.EpubBook) -> None:
         """写出img文件到SuperMemo-XML-Book文件旁边的文件夹中。"""
-        for image in self.ebook.get_items_of_type(ebooklib.ITEM_IMAGE):
+        for image in ebook.get_items_of_type(ebooklib.ITEM_IMAGE):
             # 可以得到image.file_name 和 image.content二进制数据、image.media_type
             # os.path.abspath(".")
             if not os.path.exists(self.book_img_folder):
--- tests/anki/test/web_import.py
+++ tests/anki/test/web_import.py
@@ -4,73 +4,7 @@ import requests
 from readability import Document
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
-
-
-class Gen_SM_BookXML:
-    def __init__(self, xmlname: str, savefolder=os.getcwd()):
-        self.savefolder = savefolder
-        self.xmlname = xmlname
-        self.book_file_path = os.path.join(self.savefolder, self.xmlname + ".xml")
-
-    def create_xml(self, data):
-        """输入data, 创建SuperMemo-XML-Book
-
-        Args:
-            data (_type_): _description_
-        """
-
-        root = ET.Element("SuperMemoCollection")
-
-        count = ET.SubElement(root, "Count")
-        self.set_unique_id(data)
-        count.text = str(self.count_ids(data))
-
-        for element_data in data:
-            self.create_supermemo_element(root, element_data)
-
-        tree = ET.ElementTree(root)
-        tree.write(self.book_file_path, encoding="utf-8", xml_declaration=True)
-
-    def create_supermemo_element(self, parent_element, element_data):
-        supermemo_element = ET.SubElement(parent_element, "SuperMemoElement")
-
-        for key, value in element_data.items():
-            if key == "SuperMemoElement":
-                for sub_element_data in value:
-                    self.create_supermemo_element(supermemo_element, sub_element_data)
-            elif key == "Content":
-                content = ET.SubElement(supermemo_element, "Content")
-                for sub_key, sub_value in value.items():
-                    sub_element = ET.SubElement(content, sub_key)
-                    sub_element.text = str(sub_value)
-            else:
-                sub_element = ET.SubElement(supermemo_element, key)
-                sub_element.text = str(value)
-
-    def count_ids(self, data):
-        count = 0
-        for item in data:
-            if "ID" in item:
-                count += 1
-            if "SuperMemoElement" in item:
-                count += self.count_ids(item["SuperMemoElement"])
-        return count
-
-    def set_unique_id(self, data: list):
-        """为SuperMemo元素设置id
-
-        Args:
-            data (_type_): _description_
-        """
-        count = 1
-        stack = data + []
-        while stack:
-            current_element = stack.pop()
-            current_element["ID"] = count
-            count += 1
-            if "SuperMemoElement" in current_element:
-                for sub_element in current_element["SuperMemoElement"]:
-                    stack.append(sub_element)
+from supermemo_toolkit.epub2sm.smxml import SmXml
 
 
 # with open("pathpix/weblist.txt", "r", encoding="utf-8") as f:
@@ -83,7 +17,8 @@ class Gen_SM_BookXML:
 # ]
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 "
+                  "Safari/537.36 Edg/119.0.0.0",
 }
 
 start = time.time()
@@ -119,7 +54,7 @@ for link in link_list:
     except Exception:
         pass
 
-webfile = Gen_SM_BookXML("websmxml")
+webfile = SmXml("web_sm_xml")
 end = time.time()
 print("WebI:: Done!", f" 耗时 {end - start:.2f} 秒。")
 # 根据数据结构创建XML文件
