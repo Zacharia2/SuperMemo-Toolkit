d821897857fd300fd62d95e0b30c81f27e3b0da5
XINGLAN <32425955+Zacharia2@users.noreply.github.com>
2025-02-17 14:41:01 +0800

    add sm2anki

---
+++ supermemo_toolkit/sa_sync/sm2anki.py
@@ -0,0 +1,115 @@
+import html
+from supermemo_toolkit.utilscripts.ankinet import invoke
+
+
+class s2a:
+    def __init__(self, qafile):
+        self.qafile = qafile
+        self.deckName = "TEQA Cards"
+        self.modelName = "TEQA问答题"
+
+    def setDeckName(self, deckName):
+        self.deckName = deckName
+
+    def __addNote(self, deckName: str, fields: dict):
+        try:
+            print(f"添加卡片: ENum-{fields['ENum']}")
+            invoke(
+                "addNote",
+                note={
+                    "deckName": deckName,
+                    "modelName": self.modelName,
+                    "fields": fields,
+                    "options": {
+                        "allowDuplicate": False,
+                        "duplicateScope": "deck",
+                        "duplicateScopeOptions": {
+                            "deckName": deckName,
+                            "checkChildren": False,
+                            "checkAllModels": False,
+                        },
+                    },
+                },
+            )
+        except Exception as e:
+            print(e)
+
+    def __createModel(self):
+        modelNames = invoke("modelNames")
+        if self.modelName not in modelNames:
+            print(f"创建模型: {self.modelName}")
+            invoke(
+                "createModel",
+                modelName=self.modelName,
+                inOrderFields=["Front", "Back", "ENum"],
+                css=".card {\n font-family: arial;\n font-size: 20px;\n text-align: center;\n color: black;\n background-color: white;\n}\n"
+                + ".cloze {\n color: rgb(255, 0, 0);\n background-color: rgb(255, 255, 0);\n}",
+                isCloze=False,
+                cardTemplates=[
+                    {
+                        "Name": "卡片1",
+                        "Front": "{{Front}}",
+                        "Back": "{{FrontSide}}\n\n<hr id=answer>\n\n{{Back}}",
+                    }
+                ],
+            )
+        print(f"复用模型: {self.modelName}")
+
+    def __creatDesk(self, deckName: str):
+        desk = set()
+        deckNames: str[list] = invoke("deckNames")
+        for deckname in deckNames:
+            desk.add(deckname.split("::")[0])
+        if deckName not in desk:
+            print(f"创建牌组: {deckName}")
+            invoke("createDeck", deck=deckName)
+
+    def __pre_proc(self, file: str) -> list:
+        """输入TEQA文件, 输出list[qa dict]
+
+        Args:
+            file (str): TEQA文件路径
+
+        Returns:
+            list: list[dict_qa{}]
+        """
+        with open(file, "r", encoding="GB18030") as f:
+            content = f.readlines()
+        qa_list = []
+        qa = []
+        for line in content:
+            # '<hr>\n' '\n'
+            if line != "<hr>\n" and line != "\n":
+                qa.append(line)
+            elif len(qa) != 0:
+                qa_list.append(qa)
+                qa = []
+        mqa_list = []
+        for qa in qa_list:
+            mqa = {}
+            for field in qa:
+                field: str = field.strip().split(": ", maxsplit=1)
+                name, value = field
+                mqa.update({name: html.unescape(value.strip())})
+            mqa_list.append(mqa)
+        return mqa_list
+
+    def __creat_anki_card(self, mqa_list: list):
+        # TEQA问答题, Front,Back,ENum
+        # 行是检验知的最好方式，因为知行合一
+        for qa in mqa_list:
+            if "Q" in qa and "A" in qa and "E" in qa:
+                self.__addNote(
+                    self.deckName, {"Front": qa["Q"], "Back": qa["A"], "ENum": qa["E"]}
+                )
+            elif "Q" in qa and "A" in qa:
+                self.__addNote(self.deckName, {"Front": qa["Q"], "Back": qa["A"]})
+            elif "Q" in qa:
+                self.__addNote(self.deckName, {"Front": qa["Q"]})
+        print("Done!")
+
+    def run(self):
+        self.__creatDesk(self.deckName)
+        self.__createModel()
+        qa_list = self.__pre_proc(self.qafile)
+        self.__creat_anki_card(qa_list)
