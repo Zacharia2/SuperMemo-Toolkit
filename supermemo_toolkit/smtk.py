162ef85001a1b4911513d74643e05af3909ab16e
XINGLAN <32425955+Zacharia2@users.noreply.github.com>
2025-07-11 07:04:56 +0800

    change desc

--- supermemo_toolkit/smtk.py
+++ supermemo_toolkit/smtk.py
@@ -26,7 +26,7 @@ sm_location: dict = curr_conf_dict["program"]
 @click.group()
 @click.version_option()
 def main():
-    """SuperMemo 增强工具（CLI 命令行）。包含图链整理、EPUB 图书转换导入、Latex 公式转图片等等。"""
+    """SuperMemo 增强工具(CLI命令行)。包含图链整理、EPUB图书转换导入、Latex公式转图片、sm2anki等。"""
 
 
 @click.group()
